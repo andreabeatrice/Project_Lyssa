b8d5f0c717e02103b5dbf5fa1159e656866ce457
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-08-30 17:38:08 +0200

    create code folder

---
+++ Code/Assets/Scripts/Dialogue/InteractionsCounter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is not atteched to any game object or sprite, but rather declares & instantiates global variables that must not change across scenes
+
+public static  class InteractionsCounter
+{
+   public static int receptionist = 0;
+   public static int statue = 0;
+
+   public static int mop = 0;
+
+   public static int broom = 0;
+    
+    public static int vent = 0;
+
+    public static int rat = 0;
+
+   public static int infinite = 0;
+
+   public static bool intro = false;
+    
+}
+
