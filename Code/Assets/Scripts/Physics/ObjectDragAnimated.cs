aaf244e006501ac1c4f8acbf0f6614379d47b6a4
andreabeatrice <andreaxblignaut@gmail.com>
2022-08-31 12:14:22 +0200

    add drawer #1

---
+++ Code/Assets/Scripts/Physics/ObjectDragAnimated.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDragAnimated : MonoBehaviour
+{
+
+    //onmouse drag
+    //if mouse direction down
+        //play 'open draw' animation
+    //else
+        //play ' close draw animation
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
