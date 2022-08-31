2cb1ac5c8956be8da1693d20b5780fbbe3cf01aa
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-08-31 16:05:08 +0200

    all drawers dragging

---
+++ Code/Assets/Scripts/Physics/objectDrag_nodialog.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Source: Unity Tutorial | Clicking and Dragging 2D Sprites
+//        https://youtu.be/eUWmiV4jRgU?list=PLal20IszKbo4sfOlqe1431Lx2qGBxfATK
+
+
+public class objectDrag_nodialog : MonoBehaviour
+{
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+    public AudioSource dragSound;
+    public AudioSource puttingDown;
+    public bool canDrag = false;
+
+    public Dialogue[] dialog;
+    public GameObject HeadsUpDisplay;
+
+    public string[] choices;
+
+    public GameObject[] choiceButtons = new GameObject[3];
+
+    public bool speech;
+
+    public int numInteractions = 0;
+
+    private bool bucketTalk = true;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+
+        if (Input.GetMouseButtonDown(Globals.primaryMouseButton) && canDrag && !Globals.paused)
+        {
+            dragSound.Play();
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+            startPosX = mousePos.x -  this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+            isBeingHeld = true;
+
+
+            if (HeadsUpDisplay != null)
+                HeadsUpDisplay.SetActive(true);
+
+            numInteractions++;
+
+        }
+
+    }
+
+    void OnMouseUp()
+    {
+        if (isBeingHeld){
+            puttingDown.Play();
+            isBeingHeld = false;
+            //disable click
+            //this.gameObject.transform.GetComponent<ObjectDrag>().enabled = false;
+            bucketTalk = false;
+        }
+
+    }
+
+}
