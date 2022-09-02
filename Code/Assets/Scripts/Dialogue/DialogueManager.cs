1b66a9b99160f76eaba6333d5a44ddc6c8e59acb
angela213 <u19198958@tuks.co.za>
2022-09-02 12:33:56 +0200

    x

--- Code/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Code/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -65,7 +65,11 @@ public class DialogueManager : MonoBehaviour {
 
     public void StartDialogue(Dialogue di, string[] c, GameObject[] choiceButtons, bool speech)
     {
+<<<<<<< HEAD
         if(di.voice!=null)
+=======
+        if(di.voice !=null)
+>>>>>>> d3f112d3f9d1916710f64e36c60763ba35a945fe
         {
             talking = di.voice;
         }
@@ -115,7 +119,11 @@ public class DialogueManager : MonoBehaviour {
 
     public void StartDialogue(Dialogue di, string choice, GameObject c1, bool speech)
     {
+<<<<<<< HEAD
         if (di.voice!= null)
+=======
+        if (di.voice != null)
+>>>>>>> d3f112d3f9d1916710f64e36c60763ba35a945fe
         {
             talking = di.voice;
         }
