9822dd60c9040df0114ae0cf2afee96cb8886a10
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-17 13:59:00 +0200

    add encyclopedias, start of room save system

---
+++ Code/Assets/Scripts/Save System/ObjectPositions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is not atteched to any game object or sprite, but rather declares & instantiates global variables that must not change across scenes
+
+public static  class ObjectPositions
+{
+    //Room 102
+    public static Vector2[] moveables = new Vector2[8];
+
+}
