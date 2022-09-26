6b863e5f0e81643a57c914ce033cf9194b5c6e76
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-26 20:21:23 +0200

    rat > room 106

--- Code/Assets/Scripts/Scene Scripts/Hallway_3_Mouse/Hallway_3_MouseChoiceHandler.cs
+++ Code/Assets/Scripts/Scene Scripts/Hallway_3_Mouse/Hallway_3_MouseChoiceHandler.cs
@@ -7,12 +7,19 @@ public class Hallway_3_MouseChoiceHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Globals.objectives.Dequeue();
+        HelperMethods.ObjectivesEnqueue("Don't get fired");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void enter106(){
+        FindObjectOfType<LevelLoader>().LoadNextLevel("Room106", "crossfade_start");
+
     }
 }
