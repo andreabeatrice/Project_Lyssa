fa9e0ad8efc32483985a2de5c3e7150893770187
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-27 23:08:15 +0200

    slip flip :)

--- Code/Assets/Scripts/Scene Scripts/Hallway_6_Slip/Hallway_6_Slip_Death.cs
+++ Code/Assets/Scripts/Scene Scripts/Hallway_6_Slip/Hallway_6_Slip_Death.cs
@@ -8,7 +8,8 @@ public class Hallway_6_Slip_Death : MonoBehaviour
     public AudioSources allAudio;
     public Collider2D PlayerCollider;
     public Collider2D ObjectAreaCollider;
-    public Animator watercooler_animator;
+    public Animator watercooler_animator_left, watercooler_animator_right;
+    public GameObject WaterCoolerLeft, WaterCoolerRight;
 
     private bool unplayed = true;
     
@@ -17,20 +18,29 @@ public class Hallway_6_Slip_Death : MonoBehaviour
     Color32 c = new Color32(135, 206, 253, 255);
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Hallway_6_Slip" && unplayed){
-            Debug.Log(Globals.direction);
-            watercooler_animator.Play("Player_Slip");
-            unplayed = false;
-        }
-        if (SceneManager.GetActiveScene().name != "Hallway_6_Slip" && PlayerCollider.IsTouching(ObjectAreaCollider) && !Globals.mopped)
-        {
+        if (SceneManager.GetActiveScene().name != "Hallway_6_Slip"){
             if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
                 Globals.direction = "left";
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
                 Globals.direction = "right";
             }
+        }
+        if (SceneManager.GetActiveScene().name == "Hallway_6_Slip" && unplayed){
+            if (Globals.direction == "right"){
+                WaterCoolerLeft.SetActive(false);
+                watercooler_animator_right.Play("Player_Slip");
+
+            }
+            else {
+                WaterCoolerRight.SetActive(false);
+                watercooler_animator_left.Play("Player_Slip_Reverse");
+            }
             
+            unplayed = false;
+        }
+        if (SceneManager.GetActiveScene().name != "Hallway_6_Slip" && PlayerCollider.IsTouching(ObjectAreaCollider) && !Globals.mopped){
+                        
             SceneManager.LoadScene("Hallway_6_Slip");
 
 
