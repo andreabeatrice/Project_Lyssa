88a74f6e5e074085934fbe9b99df0cef98bf7805
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-09-27 09:23:37 +0200

    slip animation
    > next, flip slip

--- Code/Assets/Scripts/Post106/watercoolerdeath.cs
+++ Code/Assets/Scripts/Post106/watercoolerdeath.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class watercoolerdeath : MonoBehaviour
 {
     public AudioSources allAudio;
     public Collider2D PlayerCollider;
     public Collider2D ObjectAreaCollider;
+    public Animator watercooler_animator;
+
+    private bool unplayed = true;
     
     //TODO: create a Global solution
     
     Color32 c = new Color32(135, 206, 253, 255);
     void Update()
     {
-        if (PlayerCollider.IsTouching(ObjectAreaCollider) && !Globals.mopped)
+        if (SceneManager.GetActiveScene().name == "Hallway_6_Slip" && unplayed){
+            unplayed = false;
+            watercooler_animator.Play("Player_WaterCooler_slip");
+        }
+        else if (SceneManager.GetActiveScene().name != "Hallway_6_Slip" && PlayerCollider.IsTouching(ObjectAreaCollider) && !Globals.mopped)
         {
 
             FindObjectOfType<LevelLoader>().LoadNextLevel("watercooler_death", "crossfade_start");
 
 
         }
+
+        
 
     }
 }
