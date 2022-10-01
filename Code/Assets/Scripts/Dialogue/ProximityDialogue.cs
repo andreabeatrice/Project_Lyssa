6affdbc5985030276aec1f8e2809095fd8edba3c
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-10-01 14:28:25 +0200

    insanity > 17

--- Code/Assets/Scripts/Dialogue/ProximityDialogue.cs
+++ Code/Assets/Scripts/Dialogue/ProximityDialogue.cs
@@ -14,13 +14,16 @@ public class ProximityDialogue : MonoBehaviour
     public bool played = false;
     public bool speech;
 
+    public bool nurse;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (Globals.blamed_receptionist){
+        if (Globals.blamed_receptionist && !nurse){
             DialogueVariable[0] = new Sentence("We run a very tight ship here, Mrs Pattel. And your incompetence this morning put our entire operation at risk.", null, "Dr Kraus", ColorCodes.kraus);
             DialogueVariable[1] = new Sentence("I'm afraid I'm going to have to ask you to pack up your desk. I want you out of here within the hour.", null, "Dr Kraus", ColorCodes.kraus);
-        } else {
+        } 
+        else if (!nurse) {
             DialogueVariable[0] = new Sentence("Mrs Pattel, I'm going to need you to speak to the Jackson Laboratory.", null, "Dr Kraus", ColorCodes.kraus);
             DialogueVariable[1] = new Sentence("JAX® Mice have told me that they will no longer ship to us. But I'm so close to my breakthrough!", null, "Dr Kraus", ColorCodes.kraus);
         }
@@ -38,7 +41,7 @@ public class ProximityDialogue : MonoBehaviour
 
             played = true;
         }
-        else if(!PlayerCollider.IsTouching(ObjectAreaCollider) && played == true){
+        else if(!PlayerCollider.IsTouching(ObjectAreaCollider) && played == true  && !nurse){
 
             played = false;
         }
---
+++ Code/Assets/Scripts/Scene Scripts/Hallway_8_insanity17/Hallway_8_Insanity17.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hallway_8_Insanity17 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D tarr;
+    public Rigidbody2D rb;
+
+    public GameObject nurseSprite;
+
+    void Start()
+    {
+            Vector3 tarrPosition = Globals.playerPositionOnMap;
+            tarrPosition.y = tarrPosition.y + 4;
+            tarrPosition.x = tarrPosition.x - 2;
+
+            Debug.Log(tarrPosition);
+            tarr.position = tarrPosition;
+            StartCoroutine(callNurse());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator callNurse(){
+        yield return new WaitForSeconds(2);
+        StartCoroutine(InsaneFernRoom());
+        nurseSprite.GetComponentsInChildren<BoxCollider2D>()[0].enabled = true;
+    }
+
+    public IEnumerator InsaneFernRoom(){
+        yield return new WaitForSeconds(4);
+        FindObjectOfType<LevelLoader>().LoadNextLevel("Room113_Fern", "crossfade_start");
+    }
+
+
+}
